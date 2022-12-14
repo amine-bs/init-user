458890277ac617da88e1f63a3e49c5466e30f60b
amine-bs <75427994+amine-bs@users.noreply.github.com>
2022-12-14 17:18:29 +0100

    Update init-user.sh

--- init-user.sh
+++ init-user.sh
@@ -1,4 +1,15 @@
+until printf "." && nc -z -w 2 localhost 3000; do
+    sleep 2;
+done;
+echo 'Metabase OK ✓' 
+
 curl -X POST \
 	-H "Content-Type: application/json"  \
 	-d '{"database": null, "prefs": { "allow_tracking": "false", "site_locale": "en", "site_name": "onyxia"}, "token": "'$MB_SETUP_TOKEN'", "user": { "email": "'$MB_ADMIN_EMAIL'", "first_name": null, "last_name": null, "password": "'$MB_ADMIN_PASSWORD'", "password_confirm": "'$MB_ADMIN_PASSWORD'", "site_name": "onyxia"}}' \
-	"localhost:3000/api/setup"
+	"localhost:3000/api/setup" &&\
+	
+echo "Initialization done!" 
+echo "sleeping..."
+while true; do
+    sleep 2
+done
