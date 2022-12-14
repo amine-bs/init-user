beeff500a909aed24b92a1c91df4bac04440ff5f
amine-bs <75427994+amine-bs@users.noreply.github.com>
2022-12-14 15:34:19 +0100

    Update init-user.sh

--- init-user.sh
+++ init-user.sh
@@ -1,4 +1,4 @@
 curl -X POST \
 	-H "Content-Type: application/json"  \
 	-d '{"database": null, "prefs": { "allow_tracking": "false", "site_locale": "en", "site_name": "onyxia"}, "token": "'$MB_SETUP_TOKEN'", "user": { "email": "'$MB_ADMIN_EMAIL'", "first_name": null, "last_name": null, "password": "'$MB_ADMIN_PASSWORD'", "password_confirm": "'$MB_ADMIN_PASSWORD'", "site_name": "onyxia"}}' \
-	"$MB_LOCAL_IP"
+	'"'$MB_LOCAL_IP'"'
