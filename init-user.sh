9114493f947de9a8762ffb749cf14de0994d998f
amine-bs <75427994+amine-bs@users.noreply.github.com>
2022-12-14 11:45:55 +0100

    Update init-user.sh

--- init-user.sh
+++ init-user.sh
@@ -1,4 +1,4 @@
 curl -X POST \
-	-H "Content-Type: application/json" \
-	-d '{"database": null, "prefs": { "allow_tracking": "false", "site_locale": "en", "site_name": "onyxia"}, "token": "metabase", "user": { "email": "toto@toto.toto", "first_name": "user", "last_name": "user", "password": "totototo1", "password_confirm": "totototo1", "site_name": "onyxia"}}' \
-	"https://cb.lab.sspcloud.fr/api/setup" 
+	-H "Content-Type: application/json"  \
+	-d '{"database": null, "prefs": { "allow_tracking": "false", "site_locale": "en", "site_name": "onyxia"}, "token": "'$MB_SETUP_TOKEN'", "user": { "email": "'$MB_ADMIN_EMAIL'", "first_name": null, "last_name": null, "password": "'$MB_ADMIN_PASSWORD'", "password_confirm": "'$MB_ADMIN_PASSWORD'", "site_name": "onyxia"}}' \
+	"localhost:3000/api/setup" 
